47b9c1e167e2879a3beca0002388f8807ba56de6
Arbaaz <arbaazahussain786@gmail.com>
2024-11-13 13:13:47 +0000

    Adding Using different version of mongoDB & Working on the collections

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const PropertiesReader = require("properties-reader");
 const cors = require("cors");
 const path = require("path");
-
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 let propertiesPath = path.resolve(__dirname, "conf/db.properties");
 
@@ -18,39 +16,120 @@ let dbparams = properties.get("db.params");
 
 // Constructing URI
 const uri = `${dbPrefix}${dbuser}:${dbpwd}${dbdbUrl}${dbparams}`;
+
+console.log(uri)
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 let client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
 let db = client.db(dbdbName);
 
 // console.log
-client
-  .connect()
-  .then(() => {
-    console.log("MongoDB connected Successfully ");
-  })
-  .catch((err) => {
-    console.error("MongoDB connected failed", err);
-    process.exit(1);
-  });
+// Asynchronous MongoDB connection using Promises
+// client
+//   .connect() // Initiates an asynchronous connection to MongoDB
+//   .then(() => {
+//     console.log("MongoDB connected Successfully ");
+//   })
+//   .catch((err) => {
+//     console.error("MongoDB connected failed", err);  // Logs error if connection fails
+//     process.exit(1); // Exits the application if the database connection fails
+//   });
 
+
+  
 
 var app = express();
 var morgan = require("morgan");
 var fs = require("fs");
 
+
+//we need this to parse json received in the requests
+//(e.g., to read json passed in req.body)
+app.use(express.json());
+
 console.log("Hello, world!");
 
 // Middleware for logging IP and request date
-app.use(function (req, res, next) {
-  console.log("Request IP: " + req.url);
-  console.log("Request date: " + new Date());
-  next();
-});
+// app.use(function (req, res, next) {
+//   console.log("Request IP: " + req.url);
+//   console.log("Request date: " + new Date());
+//   next();
+// });
 
 // Middleware for request logging with Morgan
 app.use(morgan("short"));
 
-var imagePath = path.resolve(__dirname, "public/Images");
-app.use("/images", express.static(imagePath));
+// var imagePath = path.resolve(__dirname, "public/Images");
+// app.use("/images", express.static(imagePath));
+
+// Serve static files from public and images directories
+app.use(express.static(path.resolve(__dirname, "public")));
+app.use("/images", express.static(path.resolve(__dirname, "public/Images")));
+
+// Root route
+app.get("/", (req, res) => {
+  res.send("Welcome to the homepage!");
+});
+
+// app.param middleware to dynamically set the collection based on URL
+// app.param("collectionName", (req, res, next, collectionName) => {
+//   req.collection = db.collection(collectionName);
+//   next();
+// });
+app.param('collectionName'
+  , function(req, res, next, collectionName) {
+  req.collection = db.collection(collectionName);
+   return next();
+  });
+
+// // GET route to retrieve all documents from a specific collection
+// app.get("/collections/:collectionName", (req, res, next) => {
+//   console.log("collections is connected")
+//   req.collection.find({}).toArray((err, results) => {
+//     if (err) {
+//       return next(err);
+//     }
+//     console.log(results)
+//     res.send(results);
+//   });
+// });
+
+// app.get('/collections/:collectionName'
+//   , function(req, res, next) {
+//    req.collection.find({}).toArray(function(err, results) {
+//       console.log("collections is connected")
+//    if (err) {
+//    return next(err);
+//    }
+//        console.log(results)
+//    res.send(results);
+//    });
+//   });
+
+// app.get('/collections/:collectionName', async (req, res, next) => {
+//   try {
+//     console.log("Attempting to retrieve documents from collection:", req.params.collectionName);
+//     const results = await req.collection.find({}).toArray();
+//     if (results.length === 0) {
+//       console.log("No documents found in collection:", req.params.collectionName);
+//     } else {
+//       console.log("Documents retrieved:", results);
+//     }
+//     res.send(results);
+//   } catch (err) {
+//     console.error("Error retrieving documents:", err);
+//     next(err);
+//   }
+// });
+
+app.get('/collections/:collectionName'
+  , function(req, res, next) {
+   req.collection.find({}).toArray(function(err, results) {
+   if (err) {
+   return next(err);
+   }
+   res.send(results);
+   });
+  });
 
 app.use(function (req, res) {
   res.status(404);
