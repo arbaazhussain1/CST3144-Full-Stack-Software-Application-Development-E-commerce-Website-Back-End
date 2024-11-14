b5c551a5cf485c3b658aa66df8610b0cdaa18fca
Arbaaz <arbaazahussain786@gmail.com>
2024-11-14 00:42:29 +0000

    Adding new Comments and Reformatting layout, also Removing unwanted code which is obsolete. Still Continuing with collections

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require("express");
-// const bodyParser = require("body-parser");
-const PropertiesReader = require("properties-reader");
-const cors = require("cors");
-const path = require("path");
+const express = require("express"); // Express framework for building web applications
+const PropertiesReader = require("properties-reader"); // Module to read properties file
+const cors = require("cors"); // Enable Cross-Origin Resource Sharing
+const path = require("path"); // Module to work with file paths
+const morgan = require("morgan"); // HTTP request logger middleware
+const fs = require("fs"); // File system module for working with files
 
+// Load database configuration from properties file
 let propertiesPath = path.resolve(__dirname, "conf/db.properties");
-
 let properties = PropertiesReader(propertiesPath);
 let dbPrefix = properties.get("db.prefix");
 let dbuser = properties.get("db.user");
@@ -14,127 +15,70 @@ let dbdbName = properties.get("db.dbName");
 let dbdbUrl = properties.get("db.dbUrl");
 let dbparams = properties.get("db.params");
 
-// Constructing URI
-const uri = `${dbPrefix}${dbuser}:${dbpwd}${dbdbUrl}${dbparams}`;
+// Constructing MongoDB connection URI
+const uri = `${dbPrefix}${dbuser}:${dbpwd}${dbdbUrl}/${dbdbName}${dbparams}`;
+console.log("MongoDB URI:", uri); // Log the URI to verify it's correctly constructed
 
-console.log(uri)
+// Import MongoDB client and initialize variables
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 let client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
-let db = client.db(dbdbName);
+let db; // Database variable to hold the database instance/object
 
-// console.log
 // Asynchronous MongoDB connection using Promises
-// client
-//   .connect() // Initiates an asynchronous connection to MongoDB
-//   .then(() => {
-//     console.log("MongoDB connected Successfully ");
-//   })
-//   .catch((err) => {
-//     console.error("MongoDB connected failed", err);  // Logs error if connection fails
-//     process.exit(1); // Exits the application if the database connection fails
-//   });
+client
+  .connect()
+  .then(() => {
+    db = client.db(dbdbName);
+    console.log("MongoDB connected successfully"); // Initialise db object after successful connection
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed", err); // Log connection errors
+    process.exit(1); // Exit process if connection fails
+  });
 
-
-  
-
+// Create an instance of the Express application
 var app = express();
-var morgan = require("morgan");
-var fs = require("fs");
+app.use(express.json()); // Middleware to parse JSON in requests
+app.use(morgan("short")); // Middleware for logging requests
 
-
-//we need this to parse json received in the requests
-//(e.g., to read json passed in req.body)
-app.use(express.json());
-
-console.log("Hello, world!");
-
-// Middleware for logging IP and request date
-// app.use(function (req, res, next) {
-//   console.log("Request IP: " + req.url);
-//   console.log("Request date: " + new Date());
-//   next();
-// });
-
-// Middleware for request logging with Morgan
-app.use(morgan("short"));
-
-// var imagePath = path.resolve(__dirname, "public/Images");
-// app.use("/images", express.static(imagePath));
-
-// Serve static files from public and images directories
+// Serve static files from the "public" and "public/Images" directories
 app.use(express.static(path.resolve(__dirname, "public")));
 app.use("/images", express.static(path.resolve(__dirname, "public/Images")));
 
-// Root route
+// Root route to display a welcome message
 app.get("/", (req, res) => {
-  res.send("Welcome to the homepage!");
+  res.send("Welcome to the homepage!"); // Send welcome message on root URL
 });
 
-// app.param middleware to dynamically set the collection based on URL
-// app.param("collectionName", (req, res, next, collectionName) => {
-//   req.collection = db.collection(collectionName);
-//   next();
-// });
-app.param('collectionName'
-  , function(req, res, next, collectionName) {
-  req.collection = db.collection(collectionName);
-   return next();
-  });
+// Middleware to dynamically set the collection based on URL parameter
+app.param("collectionName", function (req, res, next, collectionName) {
+  console.log("Accessing collection:", collectionName); // Log the collection name being accessed
+  req.collection = db.collection(collectionName); // Set the collection on the request object
+  next(); // Proceed to the next middleware or route handler
+});
 
-// // GET route to retrieve all documents from a specific collection
-// app.get("/collections/:collectionName", (req, res, next) => {
-//   console.log("collections is connected")
-//   req.collection.find({}).toArray((err, results) => {
-//     if (err) {
-//       return next(err);
-//     }
-//     console.log(results)
-//     res.send(results);
-//   });
-// });
+// Route to retrieve all documents from a specified collection
+app.get("/collections/:collectionName", async (req, res, next) => {
+  try {
+    console.log(
+      "Attempting to retrieve documents from collection:",
+      req.params.collectionName
+    );
+    const results = await req.collection.find({}).toArray(); // Query all documents in the collection
+    console.log("Documents retrieved:", results); // Log the documents retrieved
+    res.send(results); // Send the retrieved documents as the response
+  } catch (err) {
+    console.error("Error retrieving documents:", err); // Log any errors
+    next(err); // Pass the error to the next middleware
+  }
+});
 
-// app.get('/collections/:collectionName'
-//   , function(req, res, next) {
-//    req.collection.find({}).toArray(function(err, results) {
-//       console.log("collections is connected")
-//    if (err) {
-//    return next(err);
-//    }
-//        console.log(results)
-//    res.send(results);
-//    });
-//   });
-
-// app.get('/collections/:collectionName', async (req, res, next) => {
-//   try {
-//     console.log("Attempting to retrieve documents from collection:", req.params.collectionName);
-//     const results = await req.collection.find({}).toArray();
-//     if (results.length === 0) {
-//       console.log("No documents found in collection:", req.params.collectionName);
-//     } else {
-//       console.log("Documents retrieved:", results);
-//     }
-//     res.send(results);
-//   } catch (err) {
-//     console.error("Error retrieving documents:", err);
-//     next(err);
-//   }
-// });
-
-app.get('/collections/:collectionName'
-  , function(req, res, next) {
-   req.collection.find({}).toArray(function(err, results) {
-   if (err) {
-   return next(err);
-   }
-   res.send(results);
-   });
-  });
-
+// 404 error handler for undefined routes
 app.use(function (req, res) {
-  res.status(404);
-  res.send("File not found!");
+  res.status(404).send("File not found!");
 });
+
+// Start the server on port 3000
 app.listen(3000, function () {
   console.log("App started on port 3000");
 });
