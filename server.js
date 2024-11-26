9d97f276bea2f182de3bed380fdc45315568e963
Arbaaz <arbaazahussain786@gmail.com>
2024-11-26 16:36:14 +0000

    Adding: debugging The PUT, Now in postman it is able to edit all fields ObjectId.

--- server.js
+++ server.js
@@ -724,7 +724,7 @@ app.put("/collections/products/:id", async (req, res) => {
     const updateOperations = {};
     if ("availableInventory" in updates) {
       // Handle inventory updates separately using $inc
-      updateOperations.$inc = { availableInventory: updates.availableInventory };
+      updateOperations.$set = { availableInventory: updates.availableInventory };
     }
 
     // Handle other updates using $set
